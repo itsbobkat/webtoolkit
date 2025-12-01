267470bd2982ab5e6ad96ccd1efae5a5488c38d0
ar3ph <192461522+ar3ph@users.noreply.github.com>
2025-12-01 02:00:23 +0000

    remove runprod shell script; add static command in justfile

--- src/django_project/runprod.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# 
-# 
-# Remember to `uv run manage.py collectstatic` before running this script
-# 
-# 
-# 
-uv run gunicorn django_project.wsgi:application --workers `nproc` --worker-class gevent
